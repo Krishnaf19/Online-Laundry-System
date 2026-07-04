6ee7b9671eefb70d75d0ad6f92893664c8973904
Krishna Sonker <krishnasonker1919@gmail.com>
2026-07-04 12:46:47 +0530

    complete controller

--- src/models/store.model.js
+++ src/models/store.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import { Review } from "./review.model.js";
 
 const storeSchema = new Schema({
 
@@ -26,11 +27,38 @@ const storeSchema = new Schema({
         type: String,
         required: true
     },
-    
+
     averageRating: {
         type: Number,
         default: 0
     },
 }, { timestamps: true })
+
+
+storeSchema.statics.updateStoreReview = async function (storeId) {
+
+    const result = await Review.aggregate([
+        {
+            $match: {
+                store: new mongoose.Types.ObjectId(storeId)
+            }
+        },
+        {
+            $group: {
+                _id: "$store",              //foreignField: "localField"
+                averageRating: {
+                    $avg: "$rating"
+                }
+            }
+        }
+    ])
+
+    await this.findByIdAndUpdate(
+        storeId,
+        {
+            averageRating: result.length ? Number(result[0].averageRating.toFixed(1)) : 0
+        }
+    );
+}
 
 export const Store = mongoose.model("Store", storeSchema)
